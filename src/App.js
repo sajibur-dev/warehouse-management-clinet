923c3b84f5b467bd817924bdb6c3ebd22997576c
sajibur-dev <sajibur.dev@gmail.com>
2022-04-29 12:08:59 +0600

    basic routing set up is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+      </Routes>
     </div>
   );
 }
