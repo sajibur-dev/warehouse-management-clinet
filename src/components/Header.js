2f79a3b12c7eec1c8bc567f63ee92e6e206b4ffc
sajibur-dev <sajibur.dev@gmail.com>
2022-04-30 09:41:13 +0600

    sign out is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { NavLink } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,6 +8,9 @@ import logo from "../assets/logo/company-logo.png";
 import auth from "../firebase";
 
 const Header = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+   
   return (
       <nav className="flex justify-between items-center">
           
@@ -20,8 +24,10 @@ const Header = () => {
           <NavLink className="mr-5 uppercase" to="/myitems">My Items</NavLink>
           <NavLink className="mr-5 uppercase" to="/blogs">Blogs</NavLink>
           <NavLink className="mr-5 uppercase" to="/regester">Regester</NavLink>
-          <NavLink className="mr-5 uppercase" to="/signin">Sign In</NavLink>
-          <button onClick={() => signOut(auth)} className="mr-5 uppercase rounded-lg bg-blue-600 py-2 px-8 text-white" >sign out</button>
+
+          {!user ? <NavLink className="mr-5 uppercase rounded-lg bg-blue-600 py-3 px-8 text-white" to="/signin">Sign In</NavLink> : 
+
+          <button onClick={() => signOut(auth)} className="mr-5 uppercase rounded-lg bg-blue-600 py-2 px-8 text-white" >sign out</button>}
         </div>
       </nav>
   );
