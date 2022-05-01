9fe03d0c65da302e840b2fe7aed0d16c8094e67e
sajibur-dev <sajibur.dev@gmail.com>
2022-05-01 16:09:55 +0600

    in manage inventories pagination is done

--- src/pages/ManageInventory.js
+++ src/pages/ManageInventory.js
@@ -3,13 +3,26 @@ import { Link } from 'react-router-dom';
 
 
 const ManageInventory = () => {
-  const url = "http://localhost:5000/products";
   const [products, setProducts] = useState([]);
+  const [pageCount,setPageCount] = useState(0);
+  const [currentPage,setCurrentPage] = useState(0);
+  const [size,setSize] = useState(10);
+  const url = `http://localhost:5000/products?page=${currentPage}&&size=${size}`;
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((products) => setProducts(products));
-  }, []);
+  }, [url]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/productCount')
+    .then((res) => res.json())
+    .then((data) =>{
+      const count = data.count;
+      const pages = Math.ceil(count / 10);
+      setPageCount(pages);
+    })
+  },[])
 
   const deleteProduct = (id) => {
     const proccesed = window.confirm("Are you sure you want to delete?");
@@ -55,6 +68,11 @@ const ManageInventory = () => {
           ))}
         </tbody>
       </table>
+      <div className="flex justify-center items-center mt-5">
+        {
+          [...Array(pageCount).keys()].map((number) => <button className={`border-2 py-2 px-3 border-slate-800 mr-3 ${currentPage === number ? 'bg-slate-800 text-white' : ''}`} onClick={() => setCurrentPage(number)}>{number + 1}</button>)
+        }
+      </div>
       <div className="flex justify-end items-center mt-3">
         <Link to="/additem"
          className="py-2 px-5 bg-slate-800 rounded-lg text-white" >add items ++</Link>
